22ec5b0a4938c57e817f978744d79c6e0cc10551
Guillaume André <guillaume.andre80@gmail.com>
2019-01-12 15:49:35 +0100

    Fragment based loading

---
+++ app/src/main/java/unice/ihm/jenkins/FollowFragment.java
@@ -0,0 +1,31 @@
+package unice.ihm.jenkins;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import unice.ihm.jenkins.step.StepPagerAdapter;
+
+public class FollowFragment extends Fragment {
+
+    public static final String RECIPE_KEY = "recipe";
+
+    private StepPagerAdapter pagerAdapter;
+    private ViewPager pager;
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.follow_main, container, false);
+        Recipe recipe = (Recipe) getArguments().get(RECIPE_KEY);
+        pagerAdapter = new StepPagerAdapter(recipe, getActivity().getSupportFragmentManager());
+        pager = root.findViewById(R.id.step_pager);
+        pager.setAdapter(pagerAdapter);
+        return root;
+    }
+
+}
--- app/src/main/java/unice/ihm/jenkins/MainActivity.java
+++ app/src/main/java/unice/ihm/jenkins/MainActivity.java
@@ -1,9 +1,8 @@
 package unice.ihm.jenkins;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
-import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -11,7 +10,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
+
+import java.util.ArrayList;
+
+import unice.ihm.jenkins.step.Step;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -23,15 +25,6 @@ public class MainActivity extends AppCompatActivity
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
@@ -40,6 +33,13 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+        Fragment follow = new FollowFragment();
+        Bundle bundle = new Bundle();
+        bundle.putSerializable(FollowFragment.RECIPE_KEY, new Recipe(new ArrayList<Step>()));
+        follow.setArguments(bundle);
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.content_wrapper, follow)
+                .commit();
     }
 
     @Override
