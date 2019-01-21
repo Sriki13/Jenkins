a4fc9fa2cec1ece7a26613f8bf75a0a06fe928db
tinvernizzi <tanguy.invernizzi@yahoo.fr>
2019-01-21 10:28:49 +0100

    Minor update to the style of the main menu

--- app/src/main/java/unice/ihm/jenkins/RecipeRecyclerViewAdapter.java
+++ app/src/main/java/unice/ihm/jenkins/RecipeRecyclerViewAdapter.java
@@ -45,8 +45,6 @@ public class RecipeRecyclerViewAdapter extends RecyclerView.Adapter<RecipeRecycl
             @Override
             public void onClick(View v) {
                 if (null != mListener) {
-                    // Notify the active callbacks interface (the activity, if the
-                    // fragment is attached to one) that an item has been selected.
                     mListener.onListFragmentInteraction(holder.mItem);
                 }
             }
